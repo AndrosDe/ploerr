3f6281347948fec2059d4cb36878d8986e5ed8f1
AndrosDe <andreas_beier1981@yahoo.de>
2022-12-02 16:02:30 +0000

    created top and bottom navigation

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-   
+    path('quality/', views.quality, name='quality'),
+    path('brewery/', views.brewery, name='brewery'),
+    path('contact/', views.contact, name='contact'),
+    path('impressum/', views.impressum, name='impressum'),
+    path('agb/', views.agb, name='agb'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
     """ A view to return the index page """
-    
     return render(request, 'home/index.html')
+
+
+def quality(request):
+    """ A view to return the index page """
+    return render(request, 'home/quality.html')
+
+
+def brewery(request):
+    """ A view to return the index page """
+    return render(request, 'home/brewery.html')
+
+
+def contact(request):
+    """ A view to return the index page """
+    return render(request, 'home/contact.html')
+
+
+def impressum(request):
+    """ A view to return the index page """
+    return render(request, 'home/impressum.html')
+
+
+def agb(request):
+    """ A view to return the index page """
+    return render(request, 'home/agb.html')
