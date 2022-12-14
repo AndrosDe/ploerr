53fa4632798233e7f60f1d405bd44433ddc31e65
AndrosDe <andreas_beier1981@yahoo.de>
2022-12-14 14:26:34 +0000

    styled all allauth templates

--- ploerr/settings.py
+++ ploerr/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'home',
 ]
 
@@ -55,6 +57,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ploerr.urls'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
