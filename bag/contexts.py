4ac198ee691908dea0dcd0d722a5c351543d3ce1
AndrosDe <andreas_beier1981@yahoo.de>
2022-12-18 13:03:26 +0000

    updated the bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,16 +1,32 @@
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
+    weight = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        weight += quantity * product.total_volumen
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
+        'weight': weight,
     }
 
     return context
