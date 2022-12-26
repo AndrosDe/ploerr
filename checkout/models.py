fc1004a133d7b44dd4901032f6e17e1dcd73451d
AndrosDe <andreas_beier1981@yahoo.de>
2022-12-26 16:10:02 +0000

    tested profile and email confimation

--- checkout/models.py
+++ checkout/models.py
@@ -7,33 +7,49 @@ from django.db.models import Sum
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False)
-    full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    order_number = models.CharField(
+        max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, related_name='orders')
+    full_name = models.CharField(
+        max_length=50, null=False, blank=False)
+    email = models.EmailField(
+        max_length=254, null=False, blank=False)
+    phone_number = models.CharField(
+        max_length=20, null=False, blank=False)
+    country = CountryField(
+        blank_label='Country *', null=False, blank=False)
+    postcode = models.CharField(
+        max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(
+        max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(
+        max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    county = models.CharField(
+        max_length=80, null=True, blank=True)
+    date = models.DateTimeField(
+        auto_now_add=True)
     shipping_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
     weight_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2,
-                                      null=False, default=0)
-    deposit_total = models.DecimalField(max_digits=10, decimal_places=2,
-                                        null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
-                                      null=False, default=0)
-    original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False,
-                                  default='')
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    deposit_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(
+        null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -88,12 +104,14 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False,
-                              on_delete=models.CASCADE,
-                              related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False,
-                                on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
+    order = models.ForeignKey(
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(
+        Product, null=False, blank=False,
+        on_delete=models.CASCADE)
+    quantity = models.IntegerField(
+        null=False, blank=False, default=0)
     lineitem_total_price = models.DecimalField(
         max_digits=6, decimal_places=2,
         null=False, blank=False, editable=False)
--- ploerr/settings.py
+++ ploerr/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'profiles',
 ]
 
 MIDDLEWARE = [
