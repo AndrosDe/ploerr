c27a7cd7dd474d8f972e69f7a2af48202162037c
AndrosDe <andreas_beier1981@yahoo.de>
2022-12-28 19:16:44 +0000

    removed secret key from settings

--- ploerr/settings.py
+++ ploerr/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l=bsjza6s7y01h#)t!^2k)4n&2t3!%n(0)+p&gie#m8^pwbb4)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
