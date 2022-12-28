08994bea1d2cfd8cf8f48f58eb082c55ed71f1df
AndrosDe <andreas_beier1981@yahoo.de>
2022-12-28 19:46:46 +0000

    fixed allowed host

--- ploerr/settings.py
+++ ploerr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['androsde-ploerr.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ploerr.herokuapp.com', 'localhost']
 
 
 # Application definition
