b36a157d40adcefc34e4fa3dd5cbb097096d6601
AndrosDe <andreas_beier1981@yahoo.de>
2022-12-28 16:47:37 +0000

    added product delete view and links to edit / delete products

--- products/urls.py
+++ products/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('edit/<int:product_id>/',
          views.edit_product,
          name='edit_product'),
+    path('delete/<int:product_id>/',
+         views.delete_product,
+         name='delete_product'),
 ]
