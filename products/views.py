74689fa9642186aaae81353dccc5e0eac3936357
AndrosDe <andreas_beier1981@yahoo.de>
2023-02-08 10:52:39 +0000

    removed confirm delete product view and template

--- products/views.py
+++ products/views.py
@@ -281,22 +281,6 @@ def edit_container(request, container_id):
 
 # 4. Deleting Products, Product Descriptions & Containers
 @login_required
-def delete_product_confirm(request, product_id):
-    """ A view to confirm product deletion """
-    if not request.user.is_staff:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'products/confirm_delete_product.html', context)
-
-
-@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_staff:
